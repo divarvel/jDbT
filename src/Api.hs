2150d144d49b8689bc9ea37d3546aa325b4c8291
Julien Tanguy <julien.tanguy@jhome.fr>
2015-06-16 17:44:26 +0200

    Revamp the API server
    - Provides a GET endpoint
    
    Related to #7 Better api

--- src/Api.hs
+++ src/Api.hs
@@ -1,20 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Applicative
+-- import Control.Applicative
+-- import           Data.Maybe
+import qualified Data.Text.Lazy          as T
+-- import qualified Data.Text.Encoding as T
+import qualified Data.Text.Lazy.IO       as TL
+import qualified Data.ByteString.Lazy as BL
 import           Data.Yaml
-import QuickWebApp
+import           System.Environment
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
+import           Web.Scotty
+
 import           JDBT.Parser        ()
 import           JDBT.SQL
 
-yamlToSQL :: T.Text -> T.Text
-yamlToSQL input = either T.pack id sql
-  where
-    schema = decodeEither . T.encodeUtf8 $ input
-    sql = dataToSQL <$> schema
+import           Paths_jdbt
+
+-- yamlToSQL :: T.Text -> T.Text
+-- yamlToSQL input = either T.pack id sql
+--   where
+--     schema = decodeEither . T.encodeUtf8 $ input
+--     sql = dataToSQL <$> schema
 
 main :: IO ()
-main = interactWeb yamlToSQL
+main = do
+  port <- maybe 8080 read <$> lookupEnv "PORT"
+  home <- TL.readFile =<< getDataFileName "data/index.html"
+  scotty port $ do
+    get "/" $ html home
+    post "/" $ do
+      schema <- decodeEither . BL.toStrict <$> param "schema"
+      text $ either T.pack (T.fromStrict . dataToSQL) schema
+
